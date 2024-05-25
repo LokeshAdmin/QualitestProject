df1d86bf808b87a70e989c61173fd6b519eca29a
lokesh <lokesh@qualitestindia.com>
2024-05-25 13:20:44 +0530

    reversestring program

---
+++ RevereseString/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace RevereseString
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            String input = Console.ReadLine();
+            String output = "";
+            char[] chars = input.ToCharArray();
+            for (int  i = chars.Length-1; i>0;i--)
+            {
+                output += chars[i];
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
